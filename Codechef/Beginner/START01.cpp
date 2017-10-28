889a17fa3568440175635795f95a8e989343e405
ansumansahu <bkhanale@gmail.com>
2017-10-28 12:38:04 +0530

    clean

--- Codechef/Beginner/START01.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long int ll;
-int main()
-{
-    ll num;
-    cin>>num;
-    cout<<num<<endl;
-} 
--- Codechef/July17/Cookoff/CHNGFUNC.py
+++
@@ -1,7 +0,0 @@
-x, y = map(int, raw_input().strip().split())
-count = 0
-for i in range(1,x+1):
-    a = i*i
-    a = a + y
-    count += int(a**0.5) -i
-print count 
